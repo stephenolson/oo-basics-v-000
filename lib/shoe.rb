a764e2a9aae59a92f34375232fc37fa1c02c0b3a
Stephen Olson <stephenolson@gmail.com>
2018-10-16 20:02:30 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :brand, :page_count, :genre
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def cobble
     puts "Flipping the page...wow, you read fast!"
