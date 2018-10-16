a69c88388057ceeb482c9191e255aff09ac2f5fa
Stephen Olson <stephenolson@gmail.com>
2018-10-16 20:03:52 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,11 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brans
+  end
   
   def cobble
     puts "Flipping the page...wow, you read fast!"
